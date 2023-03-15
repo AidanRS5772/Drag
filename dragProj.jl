a991dab566dc32b82b8749ed05815fffe5cd40f8
AidanSgar <aidanrsgarlato@gmail.com>
2023-03-14 18:46:38 -0700

    implemented cubic spline with fast error analysis

--- dragProj.jl
+++ dragProj.jl
@@ -156,7 +156,7 @@ function instVals()
 
 end
 
-function plotError(dt,cutOff)
+function plotError(dt,cutOff,simX,simY,time,cnt)
     instVals()
 
     sim = quadDragSim()
@@ -168,11 +168,11 @@ function plotError(dt,cutOff)
 
     t = LinRange(0,time,floor(Int,time/dt))
     aprox = quadDragAprox(t)
-    aproxX = aprox[1]
-    aproxY = aprox[2]
+    aproxX = convert.(Float64 , aprox[1])
+    aproxY = convert.(Float64 , aprox[2])
 
-    splineX = linear_interpolation(t, aproxX)
-    splineY = linear_interpolation(t, aproxY)
+    splineX = CubicSplineInterpolation(t, aproxX)
+    splineY = CubicSplineInterpolation(t, aproxY)
     splineT = big.(collect(LinRange(0,time,cnt)))
     linAproxX = big.(collect(splineX(splineT)))
     linAproxY = big.(collect(splineY(splineT)))
@@ -196,10 +196,19 @@ function manyErrorPlots(N,b,cutOff)
     absErrors = Array{Plots.Plot, 1}(undef, N);
     relErrors = Array{Plots.Plot, 1}(undef, N);
 
+    instVals()
+
+    sim = quadDragSim()
+
+    simX = sim[1]
+    simY = sim[2]
+    time = sim[3]
+    cnt = sim[4]
+
     for i in 1:N
         dt = 10.0^(-i-b+1)
-        @printf("\n\n\n!!New Plot!!   dt:%.7f \n\n\n",dt)
-        error = plotError(dt,cutOff)
+        @printf("\n\n\n!!New Plot!!   dt:%.6f \n\n\n",dt)
+        error = plotError(dt,cutOff,simX,simY,time,cnt)
         absErrors[i] = error[1]
         relErrors[i] = error[2]
     end
@@ -218,8 +227,8 @@ function plotProj(dt)
 
     t = LinRange(0,time,floor(Int,time/dt))
     aprox = quadDragAprox(t)
-    aproxX = aprox[1]
-    aproxY = aprox[2]
+    aproxX = float(aprox[1])
+    aproxY = float(aprox[2])
 
     p = plot(simX,simY,label = "Simulation")
     return plot(p,aproxX,aproxY,label = "Aproximation")
@@ -236,5 +245,3 @@ global quadC = .25
 global D =.05
 global ep = .02
 #################################
-
-display(manyErrorPlots(3,2,.05))

5dd91119f23dea3bf1c980e376a8f6ed9ac7ab05
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2023-07-15 18:35:28 -0700

    Working On Integral Aproximation

--- test.jl
+++ test.jl
@@ -1,8 +1,9 @@
 using PlotlyJS
 using SpecialFunctions
+using QuadGK
 using TickTock
-include("projwDrag.jl")
 include("plotProjDrag.jl")
+include("projwDrag.jl")
 
 instInputs()
 s1, s2, s3 = plotProj()
@@ -10,6 +11,3 @@ p1 = plot(s1)
 p2 = plot(s2)
 p3 = plot(s3)
 [p1 p2 p3]
-
-
-
